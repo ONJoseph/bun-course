c72526df7b99c503cc84203242de3bdf21811908
onjoseph <on.josephinfo@gmail.com>
2023-10-23 13:25:54 +0100

    Added routes

--- index.ts
+++ index.ts
@@ -3,10 +3,59 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
+      const url = new URL(req.url)
+
+      if (url.pathname === '/'){
         const body = figlet.textSync("I am learning Bun!")
-      return new Response(body);
-    },
-  });
+      return new Response(body)
+    }
+    if (url.pathname === '/about'){
+      return new Response("About me!")
+    }
+
+    if (url.pathname === '/contact'){
+      return new Response('Contact Us!')
+    }
+
+    return new Response('404!')
+
+    }
+
+  })
   
   console.log(`Listening on localhost: ${server.port}`);
   
+/*
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () =>{
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='George' age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/ONJoseph'>ONJoseph</a>
+    </div>
+  )
+}
+export default App
+
+*/
